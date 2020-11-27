06ed2c1d146e68d8d3e738fa34eb9f6ab7cce0aa
Elisabeth Levana <18218032@std.stei.itb.ac.id>
2020-11-27 09:03:51 +0700

    bisa yuk

--- lib/db_conf.php
+++ lib/db_conf.php
@@ -1,10 +1,22 @@
 <?php
-    $servername = "localhost";
-    $dbname = "foodordering";
-    $username = "root";
-    $password = "";
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    <?php
+    // $servername = "localhost";
+    // $dbname = "foodordering";
+    // $username = "root";
+    // $password = "";
+ 
+      //remote
+      //$servername = "remotemysql.com";
+      //$dbname = "Z5u7cYe0sx";
+      //$username = "Z5u7cYe0sx";
+      //$password = "Qmn3HFEjJB";
+ 
+      $servername = "bkkprjl1njbxeoxjyfda-mysql.services.clever-cloud.com";
+      $dbname = "bkkprjl1njbxeoxjyfda";
+      $username = "u8moewdgzsr3zr2j";
+      $password = "nr7kaG9spizKNPpZHZ2P";
+   
+      $conn = new mysqli($servername, $username, $password, $dbname) or die("Connect failed: %s\n". $conn -> error);
 
     $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
     $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
